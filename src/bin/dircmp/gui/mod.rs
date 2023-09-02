7628154946739543d59b5a7cab29ffc19266fea5
Marcelo Lima <m-lima@users.noreply.github.com>
2023-09-02 20:35:46 +0200

    Experiment with signals

--- src/bin/dircmp/gui/mod.rs
+++ src/bin/dircmp/gui/mod.rs
@@ -9,6 +9,39 @@ pub fn run() -> std::process::ExitCode {
         println!("{path}");
     });
 
+    let ptr = ui.as_weak();
+    ui.on_load(move || {
+        println!(
+            "Out {}",
+            ptr.upgrade().map_or_else(
+                || {
+                    let mut string = slint::SharedString::new();
+                    string.push_str("Failed upgrade");
+                    string
+                    // String::from("Failed upgrade")
+                },
+                |ui| ui.get_result_path()
+            )
+        );
+        let ui = ptr.upgrade().unwrap();
+        let ptr = ui.as_weak();
+        std::thread::spawn(move || {
+            std::thread::sleep(std::time::Duration::from_secs(5));
+            println!(
+                "In {}",
+                ptr.upgrade().map_or_else(
+                    || {
+                        let mut string = slint::SharedString::new();
+                        string.push_str("Failed upgrade");
+                        string
+                        // String::from("Failed upgrade")
+                    },
+                    |ui| ui.get_result_path()
+                )
+            );
+        });
+    });
+
     ui.run().unwrap();
     0.into()
 }
