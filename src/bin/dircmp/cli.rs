b0306974b7b62927efe53efdb67640361d0e1df0
Marcelo Lima <m-lima@users.noreply.github.com>
2023-09-02 14:32:20 +0200

    WIP: Towards saving to file

--- src/bin/dircmp/cli.rs
+++ src/bin/dircmp/cli.rs
@@ -1,19 +1,30 @@
 use super::args;
 
-pub fn run(args: args::Args) -> std::process::ExitCode {
-    let start = std::time::Instant::now();
-
+pub fn run(args: args::Command) -> std::process::ExitCode {
     if let Err(e) = init_logger(args.verbosity()) {
         eprintln!("[31merror:[m {e}");
         return std::process::ExitCode::FAILURE;
     }
 
-    if let Err(e) = fallible_run(args.left, args.right, args.matched) {
-        log::error!("{e}");
-        return std::process::ExitCode::FAILURE;
+    match args {
+        args::Command::Scan(args) => {
+            let start = std::time::Instant::now();
+
+            if let Err(e) = scan(args.left, args.right, args.output, args.print) {
+                log::error!("{e}");
+                return std::process::ExitCode::FAILURE;
+            }
+
+            log::info!("Elapsed: {:?}", start.elapsed());
+        }
+        args::Command::Print(args) => {
+            if let Err(e) = print(args.input, args.matched) {
+                log::error!("{e}");
+                return std::process::ExitCode::FAILURE;
+            }
+        }
     }
 
-    log::info!("Elapsed: {:?}", start.elapsed());
     std::process::ExitCode::SUCCESS
 }
 
@@ -40,21 +51,33 @@ fn init_logger(level: log::LevelFilter) -> Result<(), log::SetLoggerError> {
     Ok(())
 }
 
-fn fallible_run(
+fn scan(
     left: std::path::PathBuf,
     right: std::path::PathBuf,
-    show_matched: bool,
+    output: Option<std::path::PathBuf>,
+    print_filter: args::PrintFilter,
 ) -> Result<(), dircmp::Error> {
     log::debug!(
-        "left: {left}, right: {right}, show_matched: {show_matched}",
+        "left: {left}, right: {right}, output: {output}, print_filter: {print_filter:?}",
         left = left.display(),
         right = right.display(),
+        output = output.map_or_else(|| String::from("<None>"), |out| out.display().to_string()),
     );
+
     let (left, right) = dircmp::compare(left, right)?;
 
-    print(&left, &right, show_matched, Mode::Left);
-    print(&right, &left, show_matched, Mode::Right);
+    match print_filter {
+        args::PrintFilter::None => {}
+        filter => {
+            write(&left, &right, filter == args::PrintFilter::All, Mode::Left);
+            write(&right, &left, filter == args::PrintFilter::All, Mode::Right);
+        }
+    }
     Ok(())
+}
+
+fn print(input: std::path::PathBuf, show_matched: bool) -> Result<(), dircmp::Error> {
+    todo!()
 }
 
 #[derive(Copy, Clone, Eq, PartialEq)]
@@ -72,27 +95,30 @@ impl std::fmt::Display for Mode {
     }
 }
 
-fn print(reference: &dircmp::Directory, other: &dircmp::Directory, show_matched: bool, mode: Mode) {
-    println!("[37mVisiting:[m {}", reference.path().display());
+fn write(reference: &dircmp::Directory, other: &dircmp::Directory, show_matched: bool, mode: Mode) {
+    use std::io::Write;
+
+    let mut out = std::io::stdout().lock();
+    writeln!(out, "[37mVisiting:[m {}", reference.path().display());
     for index in reference.entries() {
         match index.status() {
             status @ dircmp::Status::Same(_) => {
                 if show_matched && mode == Mode::Left {
-                    println!("[32m{mode} {status:<8}[m {}", index.path().display());
+                    writeln!(out, "[32m{mode} {status:<8}[m {}", index.path().display());
                 }
             }
             status @ dircmp::Status::Moved(i) => {
                 if mode == Mode::Left {
-                    println!("[33m{mode} {status:<8}[m {}", index.path().display());
-                    println!("[33m  └[m {}", unsafe {
+                    writeln!(out, "[33m{mode} {status:<8}[m {}", index.path().display());
+                    writeln!(out, "[33m  └[m {}", unsafe {
                         other.entries().get_unchecked(*i).path().display()
                     });
                 }
             }
             status @ dircmp::Status::Modified(i) => {
                 if mode == Mode::Left {
-                    println!("[35m{mode} {status:<8}[m {}", index.path().display());
-                    println!("[35m  └[m {}", unsafe {
+                    writeln!(out, "[35m{mode} {status:<8}[m {}", index.path().display());
+                    writeln!(out, "[35m  └[m {}", unsafe {
                         other.entries().get_unchecked(*i).path().display()
                     });
                 }
@@ -101,18 +127,18 @@ fn print(reference: &dircmp::Directory, other: &dircmp::Directory, show_matched:
                 let Some((tail, head)) = indices.split_last() else {
                     continue;
                 };
-                println!("[34m{mode} {status:<8}[m {}", index.path().display());
+                writeln!(out, "[34m{mode} {status:<8}[m {}", index.path().display());
                 for i in head {
-                    println!("[34m  ├[m {}", unsafe {
+                    writeln!(out, "[34m  ├[m {}", unsafe {
                         other.entries().get_unchecked(*i).path().display()
                     });
                 }
-                println!("[34m  └[m {}", unsafe {
+                writeln!(out, "[34m  └[m {}", unsafe {
                     other.entries().get_unchecked(*tail).path().display()
                 });
             }
             status @ dircmp::Status::Unique => {
-                println!("[31m{mode} {status:<8}[m {}", index.path().display());
+                writeln!(out, "[31m{mode} {status:<8}[m {}", index.path().display());
             }
         }
     }
